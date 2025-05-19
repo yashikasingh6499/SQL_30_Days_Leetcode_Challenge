d5e5f29847ad31214338c5dc2e1ccce12afcb03e
Yashika Singh <yashika6499@gmail.com>
2025-05-18 20:02:16 -0400

    Update and rename Day1.sql to Day1

--- Day1.sql
+++
@@ -1,5 +0,0 @@
-# Write your MySQL query statement below
-
-select a.name, coalesce(sum(b.distance),0) as travelled_distance
-from users a left join rides b 
-on a.id = b.user_id group by a.id, a.name order by travelled_distance desc, a.name asc;
